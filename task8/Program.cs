45c96a852de169027d58e37935f693ac780cbb43
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:36:59 +0300

    Добавьте файлы проекта.

---
+++ task8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numberOfDays = 5; // Задаем количество дней для примера
+
+        int[] temperatures = new int[numberOfDays];
+        int sum = 0;
+        int maxTemp = int.MinValue;
+        int minTemp = int.MaxValue;
+
+        for (int i = 0; i < numberOfDays; i++)
+        {
+            Console.Write($"Введите температуру для дня {i + 1}: ");
+            temperatures[i] = Convert.ToInt32(Console.ReadLine());
+            sum += temperatures[i];
+
+            if (temperatures[i] > maxTemp)
+            {
+                maxTemp = temperatures[i];
+            }
+
+            if (temperatures[i] < minTemp)
+            {
+                minTemp = temperatures[i];
+            }
+        }
+
+        double averageTemp = (double)sum / numberOfDays;
+
+        Console.WriteLine($"Средняя температура: {averageTemp}");
+        Console.WriteLine($"Максимальная температура: {maxTemp}");
+        Console.WriteLine($"Минимальная температура: {minTemp}");
+    }
+}
